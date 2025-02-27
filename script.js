d221e38199b8e27bf88e0c46ed46c97322fd43ec
rashid253 <rashidncit@gmail.com>
2025-02-27 13:06:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function toggleSideMenu() {
-  const sideMenu = document.getElementById('side-menu');
-  if (sideMenu.style.left === '0px') {
-    sideMenu.style.left = '-250px';
-  } else {
-    sideMenu.style.left = '0px';
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // Global function to open pages in a new tab/window
+  window.openPage = function(url) {
+    window.open(url, '_blank');
+  };
 
-function openPage(url) {
-  window.location.href = url;
-}
+  // Toggle mobile side menu
+  window.toggleSideMenu = function() {
+    const sideMenu = document.getElementById("side-menu");
+    if (sideMenu) {
+      sideMenu.classList.toggle("visible");
+    }
+  };
+});
